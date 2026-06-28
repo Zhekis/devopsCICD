0c282c3fa2af95aa14bf794ed23ecb3d4ff2d80c
Student <student@school21.ru>
2026-06-28 15:52:29 +0500

    workflow4

--- server/application.py
+++ server/application.py
@@ -5,26 +5,21 @@ import socketserver
 
 PORT = 8000
 
+
 class TestMe:
     """Simple class for demonstrating unit test functionality."""
-    
+
     def take_five(self):
         """Return the integer 5."""
         return 5
-    
+
     def port(self):
         """Return the server port number."""
         return PORT
-    
-    def serve_forever(self):
-        """Start the web server and serve requests indefinitely."""
-        handler = http.server.SimpleHTTPRequestHandler
-        with socketserver.TCPServer(("", PORT), handler) as httpd:
-            print("serving at port", PORT)
-            httpd.serve_forever()
+
 
 if __name__ == '__main__':
-    test_instance = TestMe()
-    test_instance.serve_forever()
-    # Web server responds to any request with print output, 
-    # by default shows client file list in current directory
+    request_handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), request_handler) as server:
+        print("serving at port", PORT)
+        server.serve_forever()
